13e095d92531ae6e5b5691ed42f18bf1c96070de
Mark Plocios <markplocios1995@gmail.com>
2026-03-19 18:45:16 +0800

    Updates

--- assets/BufferResource-yt3lOt-T.js
+++ assets/BufferResource-yt3lOt-T.js
@@ -1,4 +1,4 @@
-import{w as _,r as y,s as c,u as S,B as g,t as m,E as I,x as p}from"./index-BjKLm2PX.js";const D={name:"texture-bit",vertex:{header:`
+import{w as _,r as y,s as c,u as S,B as g,t as m,E as I,x as p}from"./index-BBKteZpl.js";const D={name:"texture-bit",vertex:{header:`
 
         struct TextureUniforms {
             uTextureMatrix:mat3x3<f32>,
