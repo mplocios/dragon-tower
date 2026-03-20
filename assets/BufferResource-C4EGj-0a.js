26233c4637f94ac9e6d37aa5962f46f56a004462
Mark Plocios <markplocios1995@gmail.com>
2026-03-20 10:22:51 +0800

    Updates

--- assets/BufferResource-C4EGj-0a.js
+++ assets/BufferResource-C4EGj-0a.js
@@ -1,4 +1,4 @@
-import{w as _,r as y,s as c,u as S,B as g,t as m,E as I,x as p}from"./index-Cj_AcR4P.js";const D={name:"texture-bit",vertex:{header:`
+import{w as _,r as y,s as c,u as S,B as g,t as m,E as I,x as p}from"./index-EyjDKD_A.js";const D={name:"texture-bit",vertex:{header:`
 
         struct TextureUniforms {
             uTextureMatrix:mat3x3<f32>,
