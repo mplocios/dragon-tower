accdcff59a7151d39a00d782f2b1b557aa22a0bc
Mark Plocios <markplocios1995@gmail.com>
2026-03-24 09:59:22 +0800

    Updates

--- assets/BufferResource-BVrcJmvM.js
+++ assets/BufferResource-BVrcJmvM.js
@@ -1,4 +1,4 @@
-import{w as _,r as y,s as c,u as S,B as g,t as m,E as I,x as p}from"./index-BQJzapH4.js";const D={name:"texture-bit",vertex:{header:`
+import{w as _,r as y,s as c,u as S,B as g,t as m,E as I,x as p}from"./index-Bx9WyV-8.js";const D={name:"texture-bit",vertex:{header:`
 
         struct TextureUniforms {
             uTextureMatrix:mat3x3<f32>,
