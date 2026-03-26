f9d3378b493fcf801e4e6c4d0536db05fa3d3393
Mark Plocios <markplocios1995@gmail.com>
2026-03-26 08:12:52 +0800

    Updates

--- assets/BufferResource-DZUsawJf.js
+++ assets/BufferResource-DZUsawJf.js
@@ -1,4 +1,4 @@
-import{w as _,r as y,s as c,u as S,B as g,t as m,E as I,x as p}from"./index-Bqzsg8sT.js";const D={name:"texture-bit",vertex:{header:`
+import{w as _,r as y,s as c,u as S,B as g,t as m,E as I,x as p}from"./index-pnzpDkY7.js";const D={name:"texture-bit",vertex:{header:`
 
         struct TextureUniforms {
             uTextureMatrix:mat3x3<f32>,
